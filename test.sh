f3860f19d4b0e602bdc476eb0bb6c7e4a5824b91
Bala Kavin Pon <bala.kavin.pon@verizon.com>
2021-02-08 08:32:58 -0800

    Pushed from Domino: https://verizon.domino-pilot.com/u/ponba/domino_test_bala/runs/6020ff2a1a1407556055604a

--- test.sh
+++ test.sh
@@ -1,1 +1,6 @@
+echo "parm 1 $1"
+echo "parm 2 $2"
+echo "parm 3 $3"
+echo "parm 4 $4"
+echo "parm 5 $5"
 papermill /mnt/code/training-Copy1.ipynb /mnt/artifacts/training-Complete.ipynb -p test_size $1 -p max_iter $2 -p l1_ratio $3 -p stat_date $4  -p end_date $5
